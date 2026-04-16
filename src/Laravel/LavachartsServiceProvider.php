e459e5c4bd54d2ecc3db5ef50b3f8956a6896229
Keith Andrews <keith.andrews@rsandh.com>
2026-04-16 10:15:33 -0400

    Changed package name
    Signed-off-by: Keith Andrews <keith.andrews@rsandh.com>

--- src/Laravel/LavachartsServiceProvider.php
+++ src/Laravel/LavachartsServiceProvider.php
@@ -45,7 +45,7 @@ class LavachartsServiceProvider extends ServiceProvider
          * If the package method exists, we're using Laravel 4
          */
         if (method_exists($this, 'package')) {
-            $this->package('khill/lavacharts');
+            $this->package('andrewskm/lavacharts');
         }
 
         include __DIR__.'/BladeTemplateExtensions.php';
