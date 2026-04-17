40cd9a337c3bc02ab8b8565274d00f477ab94557
Keith Andrews <keith.andrews@rsandh.com>
2026-04-17 12:40:43 -0400

    Updated class to include properties being defined
    Signed-off-by: Keith Andrews <keith.andrews@rsandh.com>

--- src/Lavacharts.php
+++ src/Lavacharts.php
@@ -69,6 +69,9 @@ class Lavacharts
      */
     private $scriptManager;
 
+    public ChartFactory $chartFactory;
+    public DashboardFactory $dashFactory;
+
     /**
      * Lavacharts constructor.
      */
